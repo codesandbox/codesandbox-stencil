e07b23d38b4c0a199de676dfffdae1ced2ab2ad1
Sara <hey@iamsaravieira.com>
2019-06-11 21:45:32 +0200

    start

---
+++ stencil.config.ts
@@ -0,0 +1,13 @@
+import { Config } from '@stencil/core';
+
+// https://stenciljs.com/docs/config
+
+export const config: Config = {
+  globalStyle: 'src/global/app.css',
+  globalScript: 'src/global/app.ts',
+  outputTargets: [
+    {
+      type: 'www',
+    }
+  ]
+};
